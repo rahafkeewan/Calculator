54bf70cdd9bc875886c1cce7ee65abd9baf9dfc7
rahafkeewan <Rahaf.keewan@gmail.com>
2025-01-02 23:30:45 +0300

    update

---
+++ ConsoleApp2/Calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Calculator
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Calculator(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double Add()
+        {
+            return X + Y;
+        }
+
+        public double Subtract()
+        {
+            return X - Y;
+        }
+
+        public double Multiply()
+        {
+            return X * Y;
+        }
+
+        public string Divide()
+        {
+            if (Y == 0)
+            {
+                return "Cannot divide by zero.";
+            }
+            return (X / Y).ToString();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using ConsoleApp2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the value for x:");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter the value for y:");
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        Calculator calculator = new Calculator(x, y);
+
+        Console.WriteLine("Results:");
+        Console.WriteLine($"Sum (x + y): {calculator.Add()}");
+        Console.WriteLine($"Difference (x - y): {calculator.Subtract()}");
+        Console.WriteLine($"Product (x * y): {calculator.Multiply()}");
+        Console.WriteLine($"Quotient (x / y): {calculator.Divide()}");
+    }
+}
